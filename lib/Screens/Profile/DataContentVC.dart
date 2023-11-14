f049f003f138af42e946af8fd1a21f829f48d1dc
NABEEL V A <nabeel@droobihealth.com>
2023-11-14 15:31:36 +0300

    no message

--- lib/Screens/Profile/DataContentVC.dart
+++ lib/Screens/Profile/DataContentVC.dart
@@ -634,7 +634,7 @@ class HelpBody extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Text("1. Acceptance of Terms",
+              Text("Overview",
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 20,
@@ -644,167 +644,14 @@ class HelpBody extends StatelessWidget {
         ),
         Padding(
             padding: EdgeInsets.symmetric(vertical: 0,horizontal: 15),
-            child: Text('By accessing or using the Diab-Gulzar mobile application, you agree to comply with and be bound by these Terms and Conditions. If you do not agree with any part of these terms, you may not use the App.')
+            child: Text('Diab-Gulzar is a comprehensive health management app designed to help you take control of your diabetes. Easily monitor and track essential health parameters, medications, and lifestyle habits for effective diabetic management.')
         ),
         Padding(
           padding: EdgeInsets.only(left: 15,right: 15,top: 25,bottom: 10),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Text("2. Health Information Disclaimer",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ))
-            ],
-          ),
-        ),
-        Padding(
-            padding: EdgeInsets.symmetric(vertical: 0,horizontal: 15),
-            child: Text('The App provides tools for managing health details related to diabetes, including blood sugar, blood pressure, weight, food, water intake, HbA1c, lab tests, and medication. The information provided by the App is for general informational purposes only and should not be considered as medical advice. Consult with your healthcare provider for personalized advice.')
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: 15,right: 15,top: 25,bottom: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text("3. User Account",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ))
-            ],
-          ),
-        ),
-        Padding(
-            padding: EdgeInsets.symmetric(vertical: 0,horizontal: 15),
-            child: Text('You may be required to create a user account to access certain features of the App. You are responsible for maintaining the confidentiality of your account information and for all activities that occur under your account.')
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: 15,right: 15,top: 25,bottom: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text("4. Privacy and Data Security",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ))
-            ],
-          ),
-        ),
-        Padding(
-            padding: EdgeInsets.symmetric(vertical: 0,horizontal: 15),
-            child: Text('We take your privacy seriously. Please review our Privacy Policy for information on how we collect, use, and protect your personal information.')
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: 15,right: 15,top: 25,bottom: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text("5. User Responsibilities",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ))
-            ],
-          ),
-        ),
-        Padding(
-            padding: EdgeInsets.symmetric(vertical: 0,horizontal: 15),
-            child: Text('a. You agree to provide accurate and complete information when using the App.\nb. You are solely responsible for the use of the App and any consequences that result from it.\nc. Do not share your account credentials with others.\nd. Report any unauthorized use of your account immediately.')
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: 15,right: 15,top: 25,bottom: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text("6. Medical Emergencies",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ))
-            ],
-          ),
-        ),
-        Padding(
-            padding: EdgeInsets.symmetric(vertical: 0,horizontal: 15),
-            child: Text('The App is not a substitute for professional medical advice, diagnosis, or treatment. In case of a medical emergency, contact your healthcare provider or emergency services.')
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: 15,right: 15,top: 25,bottom: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text("7. App Updates and Changes",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ))
-            ],
-          ),
-        ),
-        Padding(
-            padding: EdgeInsets.symmetric(vertical: 0,horizontal: 15),
-            child: Text('We may update or change the features of the App from time to time. You agree to accept these updates as part of your use of the App.'
-            )),
-        Padding(
-          padding: EdgeInsets.only(left: 15,right: 15,top: 25,bottom: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text("8. Termination of Access",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ))
-            ],
-          ),
-        ),
-        Padding(
-            padding: EdgeInsets.symmetric(vertical: 0,horizontal: 15),
-            child: Text('We reserve the right to terminate or suspend your access to the App without prior notice for any reason, including violation of these terms.'
-            )),
-        Padding(
-          padding: EdgeInsets.only(left: 15,right: 15,top: 25,bottom: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text("9. Limitation of Liability",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ))
-            ],
-          ),
-        ),
-        Padding(
-            padding: EdgeInsets.symmetric(vertical: 0,horizontal: 15),
-            child: Text('To the maximum extent permitted by law, the App provider shall not be liable for any direct, indirect, incidental, special, or consequential damages resulting from the use or inability to use the App.'
-            )),
-        Padding(
-          padding: EdgeInsets.only(left: 15,right: 15,top: 25,bottom: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text("10. Governing Law",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ))
-            ],
-          ),
-        ),
-        Padding(
-            padding: EdgeInsets.symmetric(vertical: 0,horizontal: 15),
-            child: Text('These terms and conditions are governed by and construed in accordance with the laws of India.'
-            )),
-        Padding(
-          padding: EdgeInsets.only(left: 15,right: 15,top: 25,bottom: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text("11. Contact Information",
+              Text("Key Features:",
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 20,
@@ -818,19 +665,133 @@ class HelpBody extends StatelessWidget {
                 child: Column(
                   children: [
                     FormattedStringWithLink(
-                        bold: "",
-                        remaining: 'For any questions or concerns regarding these terms, please contact us at ',
-                        NumberIn: '', urlString: 'tel://+919746007604', link: '+91 9746007604'),
+                        bold: "Dashboard:",
+                        remaining: "\n* Overview of your daily stats, including blood sugar levels, blood pressure, weight, and HbA1c.\n* Quick access to important sections.",
+                        NumberIn: '1.', urlString: '', link: ''),
                     FormattedStringWithLink(
-                        bold: "",
-                        remaining: 'or ',
-                        NumberIn: '', urlString: 'mailto://gulzar.nabeel@gmail.com', link: 'gulzar.nabeel@gmail.com'),
+                        bold: "Tracking:",
+                        remaining: "",
+                        NumberIn: '2.', urlString: '', link: ''),
+                    Padding(
+                        padding: EdgeInsets.symmetric(vertical: 0,horizontal:15),
+                        child: SizedBox(
+                            child: Column(
+                              children: [
+                                FormattedStringWithLink(
+                                    bold: "Blood Sugar:",
+                                    remaining: "Log your blood glucose readings throughout the day.",
+                                    NumberIn: 'a.', urlString: '', link: ''),
+                                FormattedStringWithLink(
+                                    bold: "Blood Pressure:",
+                                    remaining: "Monitor and record your blood pressure regularly.",
+                                    NumberIn: 'b.', urlString: '', link: ''),
+                                FormattedStringWithLink(
+                                    bold: "Weight:",
+                                    remaining: "Track your weight to maintain a healthy lifestyle.",
+                                    NumberIn: 'c.', urlString: '', link: ''),
+                                FormattedStringWithLink(
+                                    bold: "Food:",
+                                    remaining: "Log your meals to manage your diet effectively.",
+                                    NumberIn: 'd.', urlString: '', link: ''),
+                                FormattedStringWithLink(
+                                    bold: "Water Intake:",
+                                    remaining: "Ensure proper hydration by recording your water intake.",
+                                    NumberIn: 'e.', urlString: '', link: ''),
+                                FormattedStringWithLink(
+                                    bold: "HbA1c:",
+                                    remaining: "Keep track of your HbA1c levels for a comprehensive view of your long-term glucose control.",
+                                    NumberIn: 'f.', urlString: '', link: ''),
+                              ],
+                            ))),
+                    FormattedStringWithLink(
+                        bold: "Lab Tests:",
+                        remaining: "\nInput and track your lab test results. Stay informed about your overall health.",
+                        NumberIn: '3.', urlString: '', link: ''),
+                    FormattedStringWithLink(
+                        bold: "Medication:",
+                        remaining: "\nManage your medications with a built-in tracker. Set reminders for doses.",
+                        NumberIn: '4.', urlString: '', link: ''),
                   ],
                 ))),
         Padding(
+          padding: EdgeInsets.only(left: 15,right: 15,top: 25,bottom: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text("How to Use:",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ))
+            ],
+          ),
+        ),
+        Padding(
             padding: EdgeInsets.symmetric(vertical: 0,horizontal: 15),
-            child: Text('\n\nBy using the Diab-Gulzar App, you acknowledge that you have read, understood, and agree to be bound by these terms and conditions.\n\nLast updated: 01/01/2024\n\n\n'
-            ))
+            child: SizedBox(
+                child: Column(
+                  children: [
+                    FormattedStringWithLink(
+                        bold: "Adding Data:",
+                        remaining: "\n* Tap on the "+" icon on the dashboard to add new data.\n* Choose the category (Blood Sugar, Blood Pressure, etc.).\n* Enter the relevant details and save.",
+                        NumberIn: '1.', urlString: '', link: ''),
+                    FormattedStringWithLink(
+                        bold: "Viewing History:",
+                        remaining: "\n* Navigate to specific sections to view historical data.\n* Use the calendar to jump to a specific date.",
+                        NumberIn: '2.', urlString: '', link: ''),
+                    FormattedStringWithLink(
+                        bold: "Medication Reminders:",
+                        remaining: "\n* Set up medication reminders in the Medication section.\n* Receive notifications to take your medications on time.",
+                        NumberIn: '3.', urlString: '', link: ''),
+                    FormattedStringWithLink(
+                        bold: "Charts and Insights:",
+                        remaining: "\n* Access charts to visualize trends over time.\n* Gain insights into your health patterns.",
+                        NumberIn: '4.', urlString: '', link: ''),
+                    FormattedStringWithLink(
+                        bold: "Profile and Settings:",
+                        remaining: "\n* Personalize your profile with essential information.\n* Adjust app settings to suit your preferences.",
+                        NumberIn: '5.', urlString: '', link: '')
+                  ],
+                ))),
+        Padding(
+          padding: EdgeInsets.only(left: 15,right: 15,top: 25,bottom: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text("Tips for Effective Usage:",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ))
+            ],
+          ),
+        ),
+        Padding(
+            padding: EdgeInsets.symmetric(vertical: 0,horizontal: 15),
+            child: Text('* Regularly update your data for accurate insights.\n* Utilize reminders to stay on track with medications and lifestyle habits.\n* Review charts and trends to understand your health journey.')
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 15,right: 15,top: 25,bottom: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text("Support and Feedback:",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ))
+            ],
+          ),
+        ),
+        Padding(
+            padding: EdgeInsets.symmetric(vertical: 0,horizontal: 15),
+            child: Text('* For any questions or issues, visit our support section.\n* We value your feedback! Help us improve by sharing your thoughts.\n\n'
+            )),
+        Padding(
+            padding: EdgeInsets.symmetric(vertical: 0,horizontal: 15),
+            child: Text('Thank you for choosing Diab-Gulzar! We are here to support you on your journey to better health.\n\n\n')
+        ),
+
       ]),
     );
   }
